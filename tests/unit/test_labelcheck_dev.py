0a8848ca0dce21a2540e7412789b3da2852cf620
rkerrid <robbiejamesk@protonmail.com>
2023-10-25 15:41:22 +0200

    edited label check test

--- tests/unit/test_labelcheck_dev.py
+++ tests/unit/test_labelcheck_dev.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct 20 16:51:01 2023
+Created on Wed Oct 25 13:59:20 2023
 
-@author: rkerrid
+@author: robbi
 """
 from silac_dia_tools.label_check import file_io as io
-from silac_dia_tools.label_check import get_peptide_details 
-from silac_dia_tools.label_check import plot_spectra
+from silac_dia_tools.label_check import process_peptides as peptide
+from silac_dia_tools.label_check import plotting
 from silac_dia_tools.label_check import process_peptide_details
+import pandas as pd
+from icecream import ic
 
 # work
 path = 'C:/data/silac_dia_tools_files/data/spikein data/'
@@ -19,15 +21,15 @@ path = 'G:/My Drive/Data/data/label check test/'
 raw_files = io.list_raw_files(path)
 
 #import MQ and thermo raw file data
-msms = io.import_file(f"{path}msms.txt")
-evidence = io.import_file(f"{path}evidence.txt")
+msms = io.import_file(path + "msms.txt")
+evidence = io.import_file(path + "evidence.txt")
 ms1_dfs = io.get_raw_data(path, raw_files, 1000)
 # ic(ms1_dfs)
 
 # get peptide details
-peptides = get_peptide_details.get_most_abundant_krp_peptide(msms,evidence) 
+peptides = peptide.get_most_abundant_krp_peptide(msms,evidence) 
 
 # process details and save temp csv
 process_peptide_details.process_details(peptides, ms1_dfs, path)
 # import and plot data
-plot_spectra.import_ms_data_for_plotting(path, peptides)
+plotting.import_ms_data_for_plotting(path, peptides)
